a5ff701cfde563974137f0884fa00e30908135f5
Masum-Web-Developer <masum.diucse@gmail.com>
2021-04-20 21:27:56 +0600

    status coloring done

--- src/components/Dashboard/PackageBookingCard/PackageBookingCard.js
+++ src/components/Dashboard/PackageBookingCard/PackageBookingCard.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 const PackageBookingCard = ({booking}) => {
     const {packageName,packageImageURL,taskStatus,bookingDate}=booking;
+    let classString="btn btn-success";
+    if(taskStatus==="Pending"){
+        classString="btn btn-danger";
+    }
+    if(taskStatus==="On Going"){
+        classString="btn btn-warning text-dark";
+    }
+    const cl="btn btn-warning text-light";
     return (
         <div className="col-lg-4 col-md-6 col-sm-6  mt-4">
             <div style={{backgroundColor: '#f1f8e9' }} className="card shadow-sm border-0 text-center">
@@ -9,7 +17,7 @@ const PackageBookingCard = ({booking}) => {
                 <div className="card-body">
                     <h4 className="card-title text-center">{packageName}</h4>
                     <h6>{bookingDate}</h6>
-                    <button className="btn btn-dark ">{taskStatus}</button>
+                    <button className={classString}>{taskStatus}</button>
                 </div>
              
 
